ed2e69dede443a7b9096f4216c2fbf147539af88
Fernando Boueres <fernandoboueres.ti@gmail.com>
2024-01-26 18:30:46 -0300

    começo com o novo template

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -16,82 +16,30 @@
 
     </head>
     <body class="toggle-sidebar">
-        <header id="header" class="header fixed-top d-flex align-items-center">
-            <div class="d-flex align-items-center justify-content-between">
-                <a href="{{ route('home') }}" class="logo d-flex align-items-center">
-                    <img src="favicon.ico" alt="iAvalia Logo">
-                    <span class="d-none d-lg-block">iAvalia</span>
+      <header id="header" class="header fixed-top d-flex align-items-center">
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="{{ route('home') }}" class="logo d-flex align-items-center">
+                <img src="favicon.ico" alt="iAvalia Logo">
+                <span class="d-none d-lg-block">iAvalia</span>
+            </a>
+        </div>
+        <nav class="header-nav">
+            <ul class="d-flex align-items-center">
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                  <i class="bi bi-house"></i>
                 </a>
-            </div>
-            <nav class="header-nav">
-                <ul class="d-flex align-items-center">
-                  <li class="nav-item dropdown">
-                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                      <i class="bi bi-house"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications ">
-                      <li class="dropdown-header">
-                        Módulo de Imóveis
-                        <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">Seus Imóveis!</span></a>
-                      </li>
-                      <li>
-                        <hr class="dropdown-divider">
-                      </li>
-                      <li class="notification-item">
-                        <i class="bi bi-plus text-success"></i>
-                        <div>
-                          <h4>Novo Imóvel</h4>
-                          <p>Cadastrar Novo Imóvel</p>
-                        </div>
-                      </li>
-                      <li>
-                        <hr class="dropdown-divider">
-                      </li>
-                      <li class="notification-item">
-                        <i class="bi bi-list text-primary"></i>
-                        <div>
-                          <h4>Lista de Imóveis</h4>
-                          <p>Seus Imóveis Cadastrados</p>
-                        </div>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                      <i class="bi bi-graph-up"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications ">
-                      <li class="dropdown-header">
-                        Módulo de Avaliações
-                        <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">Suas Avaliações!</span></a>
-                      </li>
-                      <li>
-                        <hr class="dropdown-divider">
-                      </li>
-                      <li class="notification-item">
-                        <i class="bi bi-plus text-success"></i>
-                        <div>
-                          <h4>Nova Avaliação</h4>
-                          <p>Criar Nova Avaliação</p>
-                        </div>
-                      </li>
-                      <li>
-                        <hr class="dropdown-divider">
-                      </li>
-                      <li class="notification-item">
-                        <i class="bi bi-list text-primary"></i>
-                        <div>
-                          <h4>Lista de Avaliações</h4>
-                          <p>Suas Avaliações</p>
-                        </div>
-                      </li>
-                    </ul>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="nav-item">
+                    <a href="{{ route('residence.create') }}" class="nav-link"><i class="bi bi-plus"></i></a>
                   </li>
                 </ul>
-            </nav>
-        </header>
-        <main id="main" class="main">
-            {{ $slot }}
-        </main>
+              </li>
+            </ul>
+        </nav>
+      </header>
+      <main id="main" class="main">
+          {{ $slot }}
+      </main>
     </body>
 </html>
